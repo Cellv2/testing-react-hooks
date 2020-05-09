786cca8e8a02309a00d95cb3bc8c2d6da8d2b72a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-09 22:34:51 +0100

    :tada: Initial commit

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+
+}
+
+const LandingPage = (props: Props) => {
+    return (
+        <div>
+            This is the landing page
+        </div>
+    )
+}
+
+export default LandingPage
---
+++ src/components/ProfilePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+
+}
+
+const ProfilePage = (props: Props) => {
+    return (
+        <div>
+            This is the profile page
+        </div>
+    )
+}
+
+export default ProfilePage
